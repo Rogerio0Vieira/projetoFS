db0b886aa29dddc6ff52bbcc4562d86265af6397
Rogerio Vieira <rogeriovbh@gmail.com>
2021-12-01 21:57:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projetoFS
+ 
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get('', (request, response)=>{
+
+});
+
+app.post('', (request, response)=>{
+  
+});
+
+app.put('', (request, response)=>{
+  
+});
+
+app.delete('', (request, response)=>{
+  
+});
+
+app.listen(3333);
