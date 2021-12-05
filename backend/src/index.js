d488d03de108b62bc0a120ad052d9c3b0f07621a
Rogerio Vieira <rogeriovbh@gmail.com>
2021-12-05 16:21:10 -0300

    Iniciando a criação dos metodos
    Criação do método post para criação das tarefas .

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,47 @@
 const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 
-app.get('', (request, response)=>{
+const tarefas = [];
 
+app.post('/tarefas', (request, response) => {
+  const{nome, concluido} = request.body;
+
+  const tarefa = {
+    id: uuidv4(),
+    nome,
+    concluido,
+  };
+
+  tarefas.push(tarefa);
+
+  return response.status(201).json(tarefa);
 });
 
-app.post('', (request, response)=>{
+app.get('/', (request, response) => {
   
 });
 
-app.put('', (request, response)=>{
+app.post('/', (request, response) => {
   
 });
 
-app.delete('', (request, response)=>{
+app.put('/',(request, response) => {
   
 });
 
-app.listen(3333);
+app.patch('/',(request, response) => {
+  
+});
+
+app.delete('/', (request, response) => {
+  
+});
+
+module.exports = app;
